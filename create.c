d90d5b9b883a2322bf6da1ab24a1c76c7ff66cdb
amritpalsinghh <amritpal.linux@gmail.com>
2020-04-05 01:56:20 +0530

    create fuction implemented

---
+++ create.c
@@ -0,0 +1,6 @@
+#include"header.h"
+void create()
+{
+	printf("__START: %s __\r\n",__func__);
+	printf("__ENDs : %s __\r\n",__func__);
+}
---
+++ main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+	printf("___START: %s  __\r\n",__func__);
+	printf("___Ends : %s  __\r\n",__func__);
+	return 0;
+}
